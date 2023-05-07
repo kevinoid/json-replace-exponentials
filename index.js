276c4e83c5c300969e2ea8cd7c2654d173a71e6e
Kevin Locke <kevin@kevinlocke.name>
2023-05-07 15:56:33 -0600

    Style fixes for new ESLint rules
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- index.js
+++ index.js
@@ -151,5 +151,5 @@ function jsonReplaceExponentials(json, replacer) {
   }
 
   jsonWithNumberExpRE.lastIndex = 0;
-  return json.replace(jsonWithNumberExpRE, wrapReplacer);
+  return json.replaceAll(jsonWithNumberExpRE, wrapReplacer);
 };
