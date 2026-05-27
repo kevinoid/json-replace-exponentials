kevinoid/json-replace-exponentials

commit 5be08f904c5afe1c89840554f019de1d0c603304

build: Update to ESLint 9 with Flat Config
- bump @kevinoid/eslint-config from 33.0.0 to 34.0.0.
- bump eslint from ^8.38.0 to ^9.35.0, as required by peerDependencies.
- drop former peer dependencies of @kevinoid/eslint-config, which are
  now dependencies.
- add globals to devDependencies for mocha globals.
- combine .eslintrc.json and .eslintignore into eslint.config.js.
- exclude eslint.config.js from published package files.

Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

eslint.config.js
@@ -1,0 +1,71 @@
+// ESLint configuration <https://eslint.org/docs/user-guide/configuring>
+
+'use strict';
+
+const { default: nodejs } = require('@kevinoid/eslint-config/nodejs.js');
+const globals = require('globals');
+
+module.exports = [
+  {
+    ignores: [
+      'coverage/',
+      'doc/',
+    ],
+  },
+
+  ...nodejs,
+
+  {
+    rules: {
+      // Allow requiring devDependencies for build and test
+      'import/no-extraneous-dependencies': ['error', {
+        devDependencies: [
+          ...nodejs
+            .findLast(
+              (conf) => conf.rules?.['import/no-extraneous-dependencies'],
+            )
+            .rules['import/no-extraneous-dependencies'][1].devDependencies,
+          'gulpfile.js',
+          'test-bin/**',
+          'test-lib/**',
+          'test/**',
+        ],
+      }],
+
+      // Allow CommonJS modules
+      'unicorn/prefer-module': 'off',
+
+      // Don't prefer top-level await
+      // Since top-level await is only supported in ECMAScript Modules (ESM)
+      'unicorn/prefer-top-level-await': 'off',
+    },
+  },
+
+  {
+    name: 'bin config',
+    basePath: 'bin',
+    rules: {
+      // Executable scripts should have a shebang
+      'n/hashbang': 'off',
+    },
+  },
+
+  {
+    name: 'test config',
+    basePath: 'test',
+    languageOptions: {
+      globals: globals.mocha,
+    },
+    rules: {
+      // Allow, but don't require, braces around function body
+      // Braces around body of it() function is more consistent/readable
+      'arrow-body-style': 'off',
+
+      // Allow null use in tests
+      'unicorn/no-null': 'off',
+
+      // Allow EventEmitter use in tests
+      'unicorn/prefer-event-target': 'off',
+    },
+  },
+];

.eslintignore
@@ -1,3 +1,0 @@
-coverage/
-doc/
-node_modules/

commit 2019773f2bf45302e238365bee9381cfe9fce6b9

test(eslint): merge benchmark config into root
For ESLint 9 Flat Config.

Fixes: 5be08f904c5a ("build: Update to ESLint 9 with Flat Config")
Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

eslint.config.js
@@ -41,8 +41,25 @@ module.exports = [
   },
 
   {
+    name: 'benchmark config',
+    basePath: 'benchmark',
+    rules: {
+      // Disable import resolution checking since installing benchmark
+      // dependencies for linting is wasteful, particularly on CI.
+      'import/no-unresolved': 'off',
+
+      // Disable import resolution checking since installing benchmark
+      // dependencies for linting is wasteful, particularly on CI.
+      'n/no-missing-require': 'off',
+    },
+  },
+
+  {
     name: 'bin config',
-    basePath: 'bin',
+    files: [
+      'benchmark/index.js',
+      'bin/*.js',
+    ],
     rules: {
       // Executable scripts should have a shebang
       'n/hashbang': 'off',